dfd307ed4721a249f1d7ab48f705072fbea4c689
Kyle Cheung <cheung.kyle1@gmail.com>
2021-03-19 14:05:07 -0400

    Create database.sql

---
+++ database/database.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS movie_db;
+
+CREATE DATABASE movie_db;
+
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    date_added DATETIME NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    address  VARCHAR(255) NOT NULL,
+    postal VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    credit_card VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE threads (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    creation_date DATETIME NOT NULL,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE posts (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    creation_date DATETIME NOT NULL,
+    comment TEXT NOT NULL,
+    thread_id INT,
+    user_id INT,
+    FOREIGN KEY (thread_id) REFERENCES threads(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
