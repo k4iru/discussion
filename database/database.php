624ee687b2962669344b5ec6f3ad9a37b1b49ec4
stevendatle <70961170+stevendatle@users.noreply.github.com>
2021-03-31 10:33:34 -0400

    created logs folder

--- database/database.php
+++ database/database.php
@@ -15,15 +15,16 @@ class Database
     }
 
     //get pdo connection
-    public static function getDb(){
-        if(!isset(self::$dbcon)) {
+    public static function getDb()
+    {
+        if (!isset(self::$dbcon)) {
             try {
                 self::$dbcon = new \PDO(self::$dsn, self::$user, self::$pass);
                 self::$dbcon->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
                 self::$dbcon->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_OBJ);
             } catch (\PDOException $e) {
                 $msg = $e->getMessage();
-                include '../custom-error.php';
+                include 'logs/custom-error.php';
                 exit();
             }
         }
