1b2ad360ce09dd2cbb88d7a8def43e15d3eca36c
stevendatle <70961170+stevendatle@users.noreply.github.com>
2021-03-31 10:57:56 -0400

    installed composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '624ee687b2962669344b5ec6f3ad9a37b1b49ec4',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '624ee687b2962669344b5ec6f3ad9a37b1b49ec4',
+    ),
+  ),
+);
