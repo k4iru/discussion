0906aac9036852f15b5a01c85df2fa035ae1db31
Kyle Cheung <cheung.kyle1@gmail.com>
2021-04-16 19:40:10 -0400

    authentication views added

---
+++ views/login.php
@@ -0,0 +1,18 @@
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../styles/style.css">
+    <script src="scripts/script.js"></script>
+</head>
+
+<body>
+    <?php require_once 'header.php'; ?>
+    <h1>Login Page</h1>
+    <?php require_once 'footer.php'; ?>
+</body>
+
+</html>
