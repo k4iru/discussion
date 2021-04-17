751d35a8ffeb823a6357b40f61720700454e9a41
Kyle Cheung <cheung.kyle1@gmail.com>
2021-04-17 18:57:19 -0400

    Update header.php

--- views/header.php
+++ views/header.php
@@ -1,17 +1,17 @@
 <header>
     <nav class="navbar">
-        <a href="./index.php"><img src="images/newlogo.png" alt="php knights logo" class="logo" /></a>
+        <a href="./index.php"><img src="/http-5202-group/images/newlogo.png" alt="php knights logo" class="logo" /></a>
         <ul class="nav-list">
             <li class="options"><a class="links" href="#">Movies</a></li>
             <li class="options"><a class="links" href="#">People</a></li>
             <li class="options"><a class="links" href="#">Discussions</a></li>
             <li class="options"><a class="links" href="#">Profile</a></li>
-            <li class="options"><a class="links" href="views/trailers.php">Trailers</a></li>
-            <li class="options"><a class="links" href="views/posters.php">Posters</a></li>
-            <li class="options"><a class="links" href="views/review.php">Reviews</a></li>
-            <li class="options"><a class="links" href="views/user-lists.php">Lists</a></li>
+            <li class="options"><a class="links" href="/http-5202-group/views/trailers.php">Trailers</a></li>
+            <li class="options"><a class="links" href="/http-5202-group/views/posters.php">Posters</a></li>
+            <li class="options"><a class="links" href="/http-5202-group/views/review.php">Reviews</a></li>
+            <li class="options"><a class="links" href="/http-5202-group/views/user-lists.php">Lists</a></li>
         </ul>
-        <form class="search-form" action="views/search.php" method="GET">
+        <form class="search-form" action="/http-5202-group/views/search.php" method="GET">
             <input class="search-input" type="search" placeholder="Search" aria-label="Search" name="search" />
             <button class="search-btn" type="submit">Search</button>
         </form>
