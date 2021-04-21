94ce89a1b272ed2ce2d395dcb74e6a81f1887093
Steven Le <stevendatle@yahoo.com>
2021-04-21 13:51:35 -0400

    added user permissions to review
    admin can see update / delete
    user can see add

--- views/reviews/review.php
+++ views/reviews/review.php
@@ -1,21 +1,23 @@
 <?php
 
+session_start();
+
 use PhPKnights\Model\Database;
 use PhPKnights\Model\Review;
+use PhPKnights\Model\User;
 
 require_once '../../Model/Review.php';
 require_once '../../Model/Database.php';
 require_once '../../Model/User.php';
 
-session_start();
-$dbcon = Database::getDb();
-$reviewModel = new Review();
-$reviews = $reviewModel->getAllReviews(Database::getDb());
-
 if (isset($_SESSION['username'])) {
     $dbcon = Database::getDb();
     $reviewModel = new Review();
     $reviews = $reviewModel->getAllReviews(Database::getDb());
+}
+
+if (!isset($_SESSION['username'])) {
+    header('Location: ../../views/reviews/notUser.php');
 }
 
 ?>
@@ -42,8 +44,10 @@ if (isset($_SESSION['username'])) {
                     <th>Reviewed Movie</th>
                     <th>Review</th>
                     <th>Rating</th>
+                    <?php if ($_SESSION['userGroup'] == 0) {?>
                     <th>Update Review</th>
                     <th>Delete Review</th>
+                    <?php }?>
                 </tr>
             </thead>
             <tbody>
@@ -53,28 +57,33 @@ if (isset($_SESSION['username'])) {
                     <td><?=$review->review_content;?></td>
                     <td><?=$review->review_rating;?></td>
 
+                    <!-- ONLY ADMIN CAN SEE UPDATE AND DELETE FUNCTION -->
+                    <?php if ($_SESSION['userGroup'] == 0) {?>
                     <td>
                         <form action="./updateReview.php" method="post">
                             <input type="hidden" name="id" value="<?=$review->id;?>" />
                             <input type="submit" class="button" name="updateReview" value="Update" />
                         </form>
                     </td>
-
+                    <?php }?>
+                    <?php if ($_SESSION['userGroup'] == 0) {?>
                     <td>
                         <form action="./deleteReview.php" method="post">
                             <input type="hidden" name="id" value="<?=$review->id;?>" />
                             <input type="submit" class="button" name="deleteReview" value="Delete" />
                         </form>
                     </td>
+                    <?php }?>
 
                 </tr>
                 <?php }?>
             </tbody>
         </table>
-        <?php // if ($_SESSION['userGroup'] == 0) {?>
+        <!-- USER CAN SEE ADD REVIEW -->
+        <?php if ($_SESSION['userGroup'] == 1) {?>
         <!-- Add a Review -->
         <a href="./addReview.php" class="button">Add a Review</a>
-        <?php // }?>
+        <?php }?>
     </main>
     <!-- Footer -->
     <?php require_once '../footer.php'?>
