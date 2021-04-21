b7e4f5f93f1a3a6a8ebe8ee4373b852d383abd59
Adam <galekxy13@hotmail.com>
2021-04-21 14:53:24 -0400

    added basic styling to list view for list table, fixed funtionality for detail view"

--- views/list-views/user-lists.php
+++ views/list-views/user-lists.php
@@ -9,12 +9,13 @@ require_once '../../Model/User.php';
 
 session_start();
 
+$dbcon = Database::getDb();
+$listClass = new Lists();
+$lists =  $listClass->getAllLists(Database::getDb());
+$userClass = new User();
+
 if (isset($_SESSION['username'])) {
 
-    $dbcon = Database::getDb();
-    $listClass = new Lists();
-    $lists =  $listClass->getAllLists(Database::getDb());
-    $userClass = new User();
 ?>
 
 <html lang="en">
@@ -23,6 +24,7 @@ if (isset($_SESSION['username'])) {
     <meta name="description" content="User Created Movie Lists">
     <meta name="keywords" content="Movie Lists">
     <link rel="stylesheet" href="../../styles/style.css" type="text/css">
+    <link rel="stylesheet" href="../../styles/list-style.css" type="text/css">
     <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
 </head>
 
@@ -31,16 +33,16 @@ if (isset($_SESSION['username'])) {
         <?php require_once '../header.php' ?>
         
         <main id="main">
-            <p class="">List Of All User Created Lists</p>
-            <div class="">
+            <div class="container">
+                <h1 class="main-header">See What Lists Other Users have Created!</h1>
                 <!--    Displaying Data in Table-->
-                <table class="">
+                <table class="table">
                     <thead>
                     <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Creation Date</th>
+                        <!-- <th scope="col">ID</th> -->
                         <th scope="col">List Title</th>
-                        <th scope="col">User ID</th>
+                        <th scope="col">Creation Date</th>
+                        <th scope="col">Created By: </th>
                         <th scope="col">Update</th>
                         <th scope="col">Delete</th>
                         <th scope="col">Details</th>
@@ -52,22 +54,22 @@ if (isset($_SESSION['username'])) {
                         ?>
                     
                         <tr>
-                            <th><?= $list->id; ?></th>
-                            <td><?= $list->creation_date; ?></td>
-                            <td><?= $list->list_name; ?></td>
-                            <td><?= $user->username; ?></td>
-                            <td>
+                            <!-- <th><?= $list->id; ?></th> -->
+                            <td class="table-td"><?= $list->list_name; ?></td>
+                            <td class="table-td"><?= $list->creation_date; ?></td>
+                            <td class="table-td"><?= $user->username; ?></td>
+                            <td class="table-td">                                        
+                                <form action="update-list.php" method="post">
+                                <input type="hidden" name="id" value="<?= $list->id; ?>"/>
                                 <?php if($user->username == $_SESSION['username']) {
                                     ?>
-                                        <form action="update-list.php" method="post">
-                                            <input type="hidden" name="id" value="<?php $list->id; ?>"/>
                                             <input type="submit" class="" name="updateList" value="Update"/>
-                                        </form>
                                     <?php
                                 }
                                 ?>
+                                </form>
                             </td>
-                            <td>
+                            <td class="table-td">
                                 <?php if($user->username == $_SESSION['username']) {
                                         ?>
                                             <form action="delete-list.php" method="post">
@@ -78,7 +80,7 @@ if (isset($_SESSION['username'])) {
                                     }
                                 ?>
                             </td>
-                            <td>
+                            <td class="table-td">
                                 <form action="details-list.php" method="post">
                                     <input type="hidden" name="id" value="<?=  $list->id; ?>"/>
                                     <input type="submit" class="" name="detailsList" value="Details"/>
