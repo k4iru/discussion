02945382ff35137b7c34a8c88468d221e9ab3c85
bryannhughes <bryanhughes@hughesbuildingcompany.net>
2021-04-21 21:58:51 -0400

    trailers view updated

--- views/trailer-views/trailers.php
+++ views/trailer-views/trailers.php
@@ -2,7 +2,7 @@
 //Start a new server session
 session_start();
 //Include the Database and the Movie models
-use PhPKnights\Model\Database, Movie;
+use PhPKnights\Model\{Database, Trailer};
 //Autoload Documents
 require_once '../../vendor/autoload.php';
 
@@ -16,8 +16,8 @@ $id = $title = $fullTitle = "";
     <meta name="author" content="Bryan Hughes">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="Movie, Trailers, Movie Trailers">
-    <link rel="stylesheet" href="../styles/style.css" type="text/css">
-    <link rel="stylesheet" href="../styles/trailer_style.css">
+    <link rel="stylesheet" href="../../styles/style.css" type="text/css">
+    <link rel="stylesheet" href="../../styles/trailer_style.css">
     <script src="../scripts/script.js"></script>
   </head>
   <body>
@@ -31,7 +31,7 @@ $id = $title = $fullTitle = "";
         <h1>Search a Trailer</h1>
         <form action="../../scripts/TrailerScript.php" method="POST">
           <fieldset>
-            <input type="text" class="input" required>
+            <input name="trailerSearch" type="text" class="input" required>
           </fieldset>
         </form>
       </div>
@@ -46,17 +46,18 @@ $id = $title = $fullTitle = "";
         //var_dump($decodedResponse);
 
         foreach ($decodedResponse as $key => $value) {
+
           //var_dump($key, $value);
         }
         ?>
       </div>
 
 
-      </main>
+    </main>
 
     <div class="footer-container">
       <!--Footer-->
-      <?php require_once './footer.php' ?>
+      <?php require_once '../footer.php' ?>
     </div>
   </body>
 </html>
